a9fd0e4fc9ff0c406fef33774424113e68e08d8a
jihye1211 <44031523+jihye1211@users.noreply.github.com>
2020-11-11 22:02:02 +0900

    2day_hello-world

---
+++ hello-world/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import React, { Component } from 'react';
+
+class App extends Component() {
+  render() {
+    return(
+    <div>
+     <h1>Hello React</h1>
+    </div>
+  );
+}
+}
+export default App;
