7cf4dd340c1d62792a5ffda790eb3a25eb72ae46
jihye1211 <44031523+jihye1211@users.noreply.github.com>
2020-11-12 21:28:10 +0900

    JSX
    3day_JSX

--- hello_world/src/App.js
+++ hello_world/src/App.js
@@ -1,11 +1,25 @@
+import React from 'react';
 
 function App() {
+  const name = '리액트';
+
+  // const : 한번 지정하고 나면 변경이 불가능한 상수를 선언할 때 사용하는 키워드
+  // let, const는 scope가 함수 단위가 아닌 블록 단위
+  // const : 상수, let : 변수
+  // 기본적으로 const 사용, 값을 변경해야할 때 let 사용
   return (
-    <div className="App">
-      <header className="App-header">
-        <div>Hello World</div> 
-      </header>
-    </div>
+    <>
+      <h1>{name}안녕!</h1>
+      <h2>잘 작동하니?</h2>
+      
+      <div>
+      {name==='리액트'?(
+        <h1>리액트입니다.</h1>
+      ):(
+        <h2>리액트가 아닙니다.</h2>
+      )}
+      </div>
+    </>
   );
 }
 
