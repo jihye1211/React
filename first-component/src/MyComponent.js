b326ea0d9a386b911dc0f52022e9b803b830cfd7
jihye1211 <44031523+jihye1211@users.noreply.github.com>
2020-11-13 13:57:59 +0900

    component
    4day component(1)

---
+++ first-component/src/MyComponent.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const MyComponent = ({ name, favoriteNumber, children}) =>{
+    return (
+    <div>안녕하세요, 제 이름은 {name}입니다. <br/>
+    children 값은 {children}
+    입니다.
+    <br/>
+    제가 좋아하는 숫자는 {favoriteNumber}입니다.
+    </div>
+    );
+};
+
+// MyComponent를 클래스형 컴포넌트로 변환
+// class MyComponent extends Component {
+//     render() {
+//     const { name, favoriteNumber, children } = this .props； // H|구ill' 할당
+//     return (
+//     <div>
+//     안녕하세요, 제 이름은 {name}입니다. <br />
+//     children 값은 {children}
+//     입니다.
+//     <br />
+//     제가 좋아하는 숫자는 {favoriteNumber} 입니다.
+//     </div>
+//     )；
+//     }
+//     }
+
+// defalutProps : props 값을 따로 지정하지 않았을 때 보여줄 기본값.
+MyComponent.defaultProps = {
+    name: '기본 이름'
+};
+
+MyComponent.propTypes = {
+    name: PropTypes.string,
+    favoriteNumber: PropTypes.number.isRequired
+};
+
+export default MyComponent;
+// 다른 파일에서 이 파일을 import할 때, 위에서 선언한 MyComponent 클래스를 불러오도록 설정함.
