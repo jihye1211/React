5b2f9bb66ce809db2bbdded92d2b82a84ff08183
jihye1211 <44031523+jihye1211@users.noreply.github.com>
2020-11-16 23:05:26 +0900

    7day
    component- useState

---
+++ first-component/src/Say.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+
+const Say = () => {
+    const [message, setMessage] = useState('');
+    const onClickEnter =() => setMessage('안녕하세요!');
+    const onClickLeave =() => setMessage('안녕히 가세요!');
+
+    const [color, setColor] = useState('black');
+    // 한 컴포넌트에서 useState 여러번 사용하기
+    // 선언하고 밑에서 상태 지정하기
+
+    return(
+        <div>
+            <button onClick={onClickEnter}>입장</button>
+            <button onClick={onClickLeave}>퇴장</button>
+            <h1 style={{color}}>{message}</h1> 
+            {/* h1 태그에 style 속성 주기 */}
+            <button style={{color:'red'}} onClick={()=>setColor('red')}>
+                빨간색
+            </button>
+            <button style={{color:'green'}} onClick={()=>setColor('green')}>
+                초록색
+            </button>
+            <button style={{color:'blue'}} onClick={()=>setColor('blue')}>
+                파란색
+            </button>
+        </div>
+    );
+};
+
+export default Say;
